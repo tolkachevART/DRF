69ecb8835c61b0c02cc280c5b089e072591dca58
tolkachevART <168184655+tolkachevART@users.noreply.github.com>
2024-10-21 13:13:02 +0300

    истории платежей

--- users/serializers.py
+++ users/serializers.py
@@ -3,13 +3,15 @@ from rest_framework.serializers import ModelSerializer
 from users.models import User, Payment
 
 
-class UserSerializer(ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
-
-
 class PaymentSerializer(ModelSerializer):
     class Meta:
         model = Payment
         fields = "__all__"
+
+
+class UserSerializer(ModelSerializer):
+    payments = PaymentSerializer(many=True, read_only=True, source='payment_set')
+
+    class Meta:
+        model = User
+        fields = ("email", "phone", "city", "payments")
