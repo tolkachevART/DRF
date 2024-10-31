7a8b91389e1691f35a226302441cb10c013641f4
tolka <tolkachev.art@gmail.com>
2024-10-31 21:27:58 +0300

    Задание 3 Реализуйте пагинацию для вывода всех уроков и курсов.

--- materials/views.py
+++ materials/views.py
@@ -6,12 +6,14 @@ from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from materials.models import Course, Lesson, Subscription
+from materials.paginations import CustomPagination
 from materials.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer
 from users.permissions import IsModer, IsOwner
 
 
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
+    pagination_class = CustomPagination
 
     def get_queryset(self):
         if self.request.user.groups.filter(name="moders").exists():
@@ -47,7 +49,9 @@ class LessonCreateAPIView(CreateAPIView):
 
 class LessonListAPIView(ListAPIView):
     queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
     permission_classes = (IsAuthenticated, IsModer | IsOwner)
+    pagination_class = CustomPagination
 
     def get_queryset(self):
         if self.request.user.groups.filter(name="moders").exists():
