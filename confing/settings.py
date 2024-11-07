00909236ca48976ca975d96e9393836d8bf8df1e
tolka <tolkachev.art@gmail.com>
2024-11-07 12:08:30 +0300

    Задание 1 Настройте проект для работы с Celery. Также настройте приложение на работу с celery-beat для выполнения периодических задач.

--- confing/settings.py
+++ confing/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework_simplejwt',
     'drf_yasg',
+    'django_celery_beat',
     'users',
     'materials',
 
@@ -121,3 +122,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 AUTH_USER_MODEL = 'users.User'
 
 STRIPE_API_KEY = os.getenv("STRIPE_API_KEY")
+
+CELERY_TIMEZONE = TIME_ZONE
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+CELERY_BROKER_URL = os.getenv("CELERY_BROKER_URL")
+CELERY_RESULT_BACKEND = os.getenv("CELERY_RESULT_BACKEND")
